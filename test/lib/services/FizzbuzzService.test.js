e3fc5169f9e859956cb6e750325974592ce88283
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-04 12:44:05 -0500

    Paso 18: Prueba para el metodo applyValidationInExplorer -> atributo trick = score

---
+++ test/lib/services/FizzbuzzService.test.js
@@ -0,0 +1,23 @@
+
+const Reader = require('../../../lib/utils/Reader')
+const ExplorerService = require('../../../lib/services/ExplorerService')
+
+const FizzbuzzService = require('../../../lib/services/FizzbuzzService')
+
+describe('Unit Test for FizzbuzzService class', () => {
+
+    test('Test 1.- Apply validation in explorer \nFirst case: atribute trick = score', () => {
+
+        // Obtenermos los datos en formato JSON
+        const explorers = Reader.readJSONFile('explorers.json')
+
+        // Obteniendo una lista filtrada por mision
+        const explorersByMission = ExplorerService.filterByMission(explorers, 'node')
+
+        const explorerWithValidation = FizzbuzzService.applyValidationInExplorer(explorersByMission[0])
+
+        expect(explorerWithValidation.trick).toBe(1)
+
+    })
+
+})
