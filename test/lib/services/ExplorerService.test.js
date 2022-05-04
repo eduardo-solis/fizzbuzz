5aa5496ab6d674e503851adc2ef8c72791337832
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-03 22:58:35 -0500

    Paso 6: Prueba para obtener una lista con la informacion filtrada por Mision

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,23 @@
+
+// Importamos la clase que nos ayuda a leer un archivo
+const Reader = require('../../../lib/utils/Reader')
+
+// Importamos la clase ExplorerService
+const ExplorerService = require('../../../lib/services/ExplorerService')
+
+describe('Unit Test for ExplorerService class', () => {
+
+    test('Test 1.- Filter the information by Mission', () => {
+
+        // Obtenermos los datos en formato JSON
+        const explorers = Reader.readJSONFile('explorers.json')
+        //console.log(explorers)
+
+        // Obteniendo una lista filtrada por mision
+        const explorersByMission = ExplorerService.filterByMission(explorers)
+        console.log(explorersByMission)
+
+    })
+
+})
+
