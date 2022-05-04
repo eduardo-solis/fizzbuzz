f8924f0b041e114559688ac54ab5e13fbc22dccc
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-03 22:44:24 -0500

    Paso 4: Prueba para obtener la informacion de un archivo en formato JSON

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,15 @@
+
+// Importamos la clase Reader
+const Reader = require('./../../../lib/utils/Reader')
+
+describe('Unit Test for Reader class', () => {
+    
+    test('Test 1.- Get the information from a file', () => {
+
+        const informationFromFile = Reader.readJSONFile('explorers.json')
+        console.log(informationFromFile)
+
+    })
+
+})
+
