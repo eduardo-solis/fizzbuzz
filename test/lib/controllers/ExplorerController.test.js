d9ec8ea1b1463d3e4901539032d5e6c121ddbe5f
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-05 11:00:54 -0500

    Ajustando archivos con linter

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,25 +1,25 @@
 
-const ExplorerController = require('../../../lib/controllers/ExplorerController');
+const ExplorerController = require("../../../lib/controllers/ExplorerController");
 
-describe('Unit Test for ExplorerController class', () => {
+describe("Unit Test for ExplorerController class", () => {
 
-    test('Test 1.- Get a list of explorer by mission', () => {
+    test("Test 1.- Get a list of explorer by mission", () => {
 
-        const explorers = ExplorerController.getExplorersByMission('node');
+        const explorers = ExplorerController.getExplorersByMission("node");
         expect(explorers).not.toHaveLength(0);
 
     });
 
-    test('Test 2.- Validation to get a list of explorers by mission', () => {
+    test("Test 2.- Validation to get a list of explorers by mission", () => {
 
-        const explorers = ExplorerController.getExplorersByMission('nodes');
+        const explorers = ExplorerController.getExplorersByMission("nodes");
         expect(explorers.error).toMatch(/no existen explorers con esta mision/);
 
     });
 
     test("Test 3.- Get a list of explorer's usernames by mission", () => {
 
-        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission('node');
+        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission("node");
         //console.log(explorersUsernames)
         expect(explorersUsernames).not.toHaveLength(0);
 
@@ -27,7 +27,7 @@ describe('Unit Test for ExplorerController class', () => {
 
     test("Test 4.- Validation to get a list of explorer's usernames by mission", () => {
 
-        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission('nodes');
+        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission("nodes");
         //console.log(explorersUsernames)
         expect(explorersUsernames.error).toMatch(/no existen explorers con esta mision/);
 
@@ -35,17 +35,17 @@ describe('Unit Test for ExplorerController class', () => {
 
     test("Test 5.- Get the quantity of explorers by mission", () => {
 
-        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission('node');
+        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission("node");
         expect(typeof (quantityOfExplorers)).toBe( typeof (0) );
 
     });
 
     test("Test 6.- Validation to get the quantity of explorers by mission", () => {
 
-        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission('nodes');
+        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission("nodes");
         expect(quantityOfExplorers.error).toMatch(/no existen explorers con esta mision/);
 
     });
 
-})
+});
 
