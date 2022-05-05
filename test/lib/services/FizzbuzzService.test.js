e7f5690d8931a40e42fe341db2cd17e00c69c894
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-05 11:12:02 -0500

    Paso 41: Prueba para el metodo applyValidationInNumber

--- test/lib/services/FizzbuzzService.test.js
+++ test/lib/services/FizzbuzzService.test.js
@@ -48,4 +48,32 @@ describe("Unit Test for FizzbuzzService class", () => {
 
     });
 
+    test('Test 5.- Apply validation in number\n First case: return number', () => {
+        
+        const result =  FizzbuzzService.applyValidationInNumber(1);
+        expect(result).toBe(1);
+
+    });
+
+    test('Test 6.- Apply validation in number\n First case: return "FIZZ"', () => {
+        
+        const result =  FizzbuzzService.applyValidationInNumber(3);
+        expect(result).toBe("FIZZ");
+
+    });
+
+    test('Test 7.- Apply validation in number\n First case: return "BUZZ"', () => {
+        
+        const result =  FizzbuzzService.applyValidationInNumber(5);
+        expect(result).toBe("BUZZ");
+
+    });
+
+    test('Test 8.- Apply validation in number\n First case: return "FIZZBUZZ"', () => {
+        
+        const result =  FizzbuzzService.applyValidationInNumber(15);
+        expect(result).toBe("FIZZBUZZ");
+
+    });
+
 });
