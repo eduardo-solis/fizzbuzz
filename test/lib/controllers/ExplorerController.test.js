b57057724b81774ec0de20c1f7f0589f9c83c05f
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-05 10:28:01 -0500

    Paso 35: Prueba para el metodo getExplorersAmonutByMission

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -20,7 +20,7 @@ describe('Unit Test for ExplorerController class', () => {
     test("Test 3.- Get a list of explorer's usernames by mission", () => {
 
         const explorersUsernames = ExplorerController.getExplorersUsernamesByMission('node');
-        console.log(explorersUsernames)
+        //console.log(explorersUsernames)
         expect(explorersUsernames).not.toHaveLength(0);
 
     });
@@ -28,8 +28,22 @@ describe('Unit Test for ExplorerController class', () => {
     test("Test 4.- Validation to get a list of explorer's usernames by mission", () => {
 
         const explorersUsernames = ExplorerController.getExplorersUsernamesByMission('nodes');
-        console.log(explorersUsernames)
+        //console.log(explorersUsernames)
         expect(explorersUsernames.error).toMatch(/no existen explorers con esta mision/);
+
+    });
+
+    test("Test 5.- Get the quantity of explorers by mission", () => {
+
+        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission('node');
+        expect(typeof (quantityOfExplorers)).toBe( typeof (0) );
+
+    });
+
+    test("Test 6.- Validation to get the quantity of explorers by mission", () => {
+
+        const quantityOfExplorers = ExplorerController.getExplorersAmonutByMission('nodes');
+        expect(quantityOfExplorers.error).toMatch(/no existen explorers con esta mision/);
 
     });
 
