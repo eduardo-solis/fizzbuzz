1de043aa3ee20898eeff993d42f7ea01d4bddee5
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-05 09:55:45 -0500

    Corrigiendo la prueba

--- test/lib/controllers/ExplorerController.js
+++ test/lib/controllers/ExplorerController.js
@@ -5,14 +5,14 @@ describe('Unit Test for ExplorerController class', () => {
 
     test('Test 1.- Get a list of explorer by mission', () => {
 
-        const explorers = ExplorerController('node');
+        const explorers = ExplorerController.getExplorersByMission('node');
         expect(explorers).not.toHaveLength(0);
 
     });
 
     test('Test 2.- Validation to get a list of explorers by mission', () => {
 
-        const explorers = ExplorerController('nodes');
+        const explorers = ExplorerController.getExplorersByMission('nodes');
         expect(explorers.error).toMatch(/no existen explorers con esta mision/);
 
     });
